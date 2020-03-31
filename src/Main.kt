bd7895724d401d889403026f75566d0ee7631ab8
LBR2048 <leo.ardjomand@gmai.com>
2020-03-31 14:13:10 +0200

    Add time complexity to documentation

--- src/Main.kt
+++ src/Main.kt
@@ -3,15 +3,11 @@
  * Lower and uppercase characters are considered to be different.
  * Whitespaces, line breaks and tabs are not considered valid characters.
  *
- * This solution is possibly slower than using an array of bins for each character,
- * but has the advantage of working with all Unicode characters without the need
- * to specify a bin for each one of them.
- * For uses such as DNA sequence analysis, which has only 4 different letters,
- * a bin-based approach is probably faster.
+ * The time complexity is O(n)
  */
 fun CharSequence.findFirstUniqueCharacter(): Char? {
 
-    // Create LinkedHashMap that relates char to its count, keeping insertion order
+    // Create linked hash map that relates char to its count, keeping insertion order
     val charCountMap = linkedMapOf<Char, Int>().toMutableMap()
     forEach {
         if (it.isWhitespace()) return@forEach
